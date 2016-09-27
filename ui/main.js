bbabc473d5783bebe01c5e8da082b9cea685c30a
MonuKumar0 <kumarmonu0522@gmail.com>
2016-09-28 02:36:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ k.innerHTML="Hi,This is Monu Kumar";
 var img=document.getElementById('im');
 var margin=100;
 img.onClick=function (){
-img.style.marginLeft="200px";
+img.style.marginLeft='200px';
 
 };
