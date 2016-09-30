ca9d2429f1f18284150398e8245da0463df3348f
MonuKumar0 <kumarmonu0522@gmail.com>
2016-09-30 06:17:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,8 +15,22 @@ var interval=setInterval(move,50);
 var counter=0;
 var button=document.getElementById("b");
 b.onclick=function(){
-    counter=counter+1;
-    var s=document.getElementById("s");
-    s.innerHTML=" "+counter.toString()+"times";
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+    if(request.readystate===XMLHttpRequest.DONE){
+        if(request.status===200){
+            var names=["name1","name2","name3"];
+            var list='';
+            for(var i=0;i<name.length;i++){
+                list+="<li>"+names[i]+"</li>";
+            }
+            var ul=document.getElementById("namelist");
+            ul.innerHTML=list;
+            
+        }
+    }
+}
 };
+
+
 
