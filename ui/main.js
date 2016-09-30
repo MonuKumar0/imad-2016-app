3a460927bba0db4c766166d4d85aba03b068fec1
MonuKumar0 <kumarmonu0522@gmail.com>
2016-09-30 06:30:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,11 +15,15 @@ var interval=setInterval(move,50);
 var counter=0;
 var button=document.getElementById("b");
 b.onclick=function(){
+    var nameInput=document.getElementById("name");
+    var name=nameInput.value;
+    
 var request=new XMLHttpRequest();
 request.onreadystatechange=function(){
     if(request.readystate===XMLHttpRequest.DONE){
         if(request.status===200){
-            var names=["name1","name2","name3"];
+            var names=request.responseText;
+            names=JSON.parse(names);
             var list='';
             for(var i=0;i<name.length;i++){
                 list+="<li>"+names[i]+"</li>";
@@ -29,7 +33,9 @@ request.onreadystatechange=function(){
             
         }
     }
-}
+};
+request.open("GET","http://monukumar0.imad.hasura-app.io/submit-name?name"+name,true);
+request.send(null);
 };
 
 
