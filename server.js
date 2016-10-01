069b210147321d3430f381903b5cbcabf0e9ba09
MonuKumar0 <kumarmonu0522@gmail.com>
2016-10-01 07:03:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/ui/IMG_20160524_001023.jpg', function (req, res) {
 });
 var names=[""];
 app.get("/submit-name",function(req,res){
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
