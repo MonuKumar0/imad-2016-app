5f9e455d04d5ff56b4897082462464eeebb526f2
MonuKumar0 <kumarmonu0522@gmail.com>
2016-10-05 15:27:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/pieces_chess_boards_glass_68430_3840x2160.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pieces_chess_boards_glass_68430_3840x2160.jpg'));
+});
 
 app.get('/ui/IMG_20160524_001023.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'IMG_20160524_001023.jpg'));
