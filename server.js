61f585ada98fa65303a75d7eefb7c8712dd96a28
MonuKumar0 <kumarmonu0522@gmail.com>
2016-10-24 04:29:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var pool=new Pool(config);
-app.get('/articles/:art',function(req,res){
+app.get('/articles/:art', function(req,res){
     var name=req.params.art;
-    pool.query('select * from articles where name='+name,function(err,result)
+    pool.query('select * from articles where name='+name, function(err,result)
     {
         if(err){
             res.status(500).send(err.toString());
