67e09b0635852d2b1a178542bd2f8926492468d0
MonuKumar0 <kumarmonu0522@gmail.com>
2016-10-24 17:51:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,13 +22,17 @@ app.get('/articles/:art', function(req,res){
     
     pool.query("select * from articles where name='article_one';", function(err,result)
     {
-        if(err){
+        if(err)
+        {
             res.status(500).send(err.toString());
         }
         else {
-            if(result.rows.length===0){
+            if(result.rows.length===0)
+            {
                 res.status(404).send('Article Not Found');
-            }else{
+            }
+            else
+            {
                 var articledata=result.rows[0];
                 res.send(createTemplate(articledata));
             }
