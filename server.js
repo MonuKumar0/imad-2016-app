f430be5789d8170e74dccbf6b46824c027b0543a
MonuKumar0 <kumarmonu0522@gmail.com>
2016-10-25 17:21:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 var pool=new Pool(config);
 app.get('/articles', function(req,res){
     
-    pool.query("select * from articles", function(err,result)
+    pool.query("select * from 'articles'", function(err,result)
     {
         if(err)
         {
