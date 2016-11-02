b582969ee6e1bf249b24f19c5eacd0b0b94fc7c9
MonuKumar0 <kumarmonu0522@gmail.com>
2016-11-02 19:31:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ bar.style.top="237px";
     di.style.backgroundColor="AliceBlue";
 };
 vid.onclick=function(){
-bar.style.top="287px";
+bar.style.top="284px";
      di.innerHTML="";
     di.style.border="thick solid blue";
 
