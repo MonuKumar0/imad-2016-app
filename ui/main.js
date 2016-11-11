f5b6dad72d89e69d361a8c135d820a117be46302
MonuKumar0 <kumarmonu0522@gmail.com>
2016-11-11 16:54:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ bar.style.top="237px";
 };
 vid.onclick=function(){
 bar.style.top="278px";
-     di.innerHTML="<div style=position:absolute;width:225px;height:150px;top:20px;left:20px;background-color:red;></div><div style=position:absolute;width:225px;height:150px;top:20px;left:260px;background-color:red;></div><div style=position:absolute;width:225px;height:150px;top:20px;left:500px;background-color:red;></div>";
+     di.innerHTML="<div style=position:absolute;width:225px;height:150px;top:20px;left:20px;><iframe width=225px height=150px src='https://www.youtube.com/embed/YC__12iAJGc'></iframe></div><div style=position:absolute;width:225px;height:150px;top:20px;left:260px;background-color:red;></div><div style=position:absolute;width:225px;height:150px;top:20px;left:500px;background-color:red;></div>";
     di.style.border="thick solid aqua";
 
     di.style.backgroundColor="AliceBlue";
